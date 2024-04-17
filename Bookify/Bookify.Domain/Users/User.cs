64f01df19dcebabe5bd6149ed1ea6abb987a8a6c
Miguel Fortes <fortesmiguel81@gmail.com>
2024-04-18 00:50:59 +0100

    added UserCreatedDomainEvent

--- Bookify/Bookify.Domain/Users/User.cs
+++ Bookify/Bookify.Domain/Users/User.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
 
 namespace Bookify.Domain.Users;
 
@@ -21,6 +22,8 @@ public sealed class User : Entity
     public static User Create(FirstName firstName, LastName lastName, Email email)
     {
         var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+        user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
 
         return user;
     }
