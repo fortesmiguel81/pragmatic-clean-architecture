839c34b3ff94cc77c4e7e892018a764b5d41757a
fortesmiguel81 <fortesmiguel81@gmail.com>
2024-04-23 12:44:33 +0100

    added LoggingBehaviour and ValidationBehaviour to the request pipeline

---
+++ Bookify/Bookify.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Application.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(IEnumerable<ValidationError> validationErrors)
+    {
+        Errors = validationErrors;
+    }
+
+    public IEnumerable<ValidationError> Errors { get; }
+}
