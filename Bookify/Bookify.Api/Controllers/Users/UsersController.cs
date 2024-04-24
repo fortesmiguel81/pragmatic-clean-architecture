aa76f6bd806e97c250bbeea2f671fd18eeaf165f
fortesmiguel81 <fortesmiguel81@gmail.com>
2024-04-24 19:00:49 +0100

    fixed UsersController route bug

--- Bookify/Bookify.Api/Controllers/Users/UsersController.cs
+++ Bookify/Bookify.Api/Controllers/Users/UsersController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Api.Controllers.Users;
 
-[Route("api/user")]
+[Route("api/users")]
 [ApiController]
 public class UsersController : ControllerBase
 {
