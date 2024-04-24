ac0311c1a2f9ef2632ddf8f8c81f634486491108
fortesmiguel81 <fortesmiguel81@gmail.com>
2024-04-24 17:50:29 +0100

    added Authentication configuration

---
+++ Bookify/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Infrastructure.Authentication;
+
+public class AuthenticationOptions
+{
+    public string Audience { get; init; } = string.Empty;
+
+    public string MetadataUrl { get; init; } = string.Empty;
+
+    public bool RequireHttpsMetadata { get; init; }
+
+    public string Issuer { get; set; } = string.Empty;
+}
