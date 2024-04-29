6eee198d2e08cb8cb258c93276393a046ac2ec6d
fortesmiguel81 <fortesmiguel81@gmail.com>
2024-04-29 19:15:59 +0100

    implemented the user login feature

--- Bookify/Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify/Bookify.Infrastructure/DependencyInjection.cs
@@ -32,6 +32,13 @@ public static class DependencyInjection
 
         AddPersistence(services, configuration);
 
+        AddAuthentication(services, configuration);
+
+        return services;
+    }
+
+    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
         services
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer();
@@ -52,7 +59,12 @@ public static class DependencyInjection
             })
             .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
 
-        return services;
+        services.AddHttpClient<IJwtService, JwtService>((serviceProvider, httpClient) =>
+        {
+            var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
+        });
     }
 
     private static void AddPersistence(IServiceCollection services, IConfiguration configuration)
