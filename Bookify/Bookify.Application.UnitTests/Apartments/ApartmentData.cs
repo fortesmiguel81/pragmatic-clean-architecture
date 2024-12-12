9663f855794d55ffb5fe5fe930ea4dc717ccda93
fortesmiguel81 <fortesmiguel81@gmail.com>
2024-12-12 16:50:05 +0000

    test(applicationtests):
    added more application tests

---
+++ Bookify/Bookify.Application.UnitTests/Apartments/ApartmentData.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.UnitTests.Apartments;
+
+internal class ApartmentData
+{
+    public static Apartment Create() => new(
+        Guid.NewGuid(),
+        new Name("Test Apartment"),
+        new Description("Test Description"),
+        new Address("Country", "State", "ZipCode", "City", "Street"),
+        new Money(100.0m, Currency.Usd),
+        Money.Zero(),
+        []);
+}
---
+++ Bookify/Bookify.Application.UnitTests/Users/UserData.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Application.UnitTests.Users;
+
+internal static class UserData
+{
+    public static User Create() => User.Create(FirstName, LastName, Email);
+
+    public static readonly FirstName FirstName = new("John");
+    public static readonly LastName LastName = new("Doe");
+    public static readonly Email Email = new("test@test.com");
+}
